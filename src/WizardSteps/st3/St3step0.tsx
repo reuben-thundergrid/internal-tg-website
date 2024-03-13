ae7bc04b49dc10c7e9cc36e692d2e1460523c42a
Reuben 'Aurah' Harris <AurahTF@users.noreply.github.com>
2024-03-13 15:09:59 +1300

    resolve2

--- src/WizardSteps/st3/St3step0.tsx
+++ src/WizardSteps/st3/St3step0.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import St3step0 from "../../Gif/st3step0.gif";
+import st3step0Gif from "../../Gif/st3step0.gif";
 import StepHeader from "../../Components/StepHeader.tsx";
 
 import Button from '@mui/material/Button';
@@ -16,7 +16,7 @@ export default (props: {
     <>
     <StepHeader>create a subtask inside <i>Dormant</i></StepHeader>
     <div style={{display: "flex", marginLeft: "auto", alignContent: "center", justifyContent: "center", alignSelf: "center"}}>
-      <img style={{gridRowStart: 2, gridRowEnd: 3, gridColumnStart: 2, gridColumnEnd: 3, margin: "1em", borderRadius: "1em"}} src={St3step0}/>
+      <img style={{gridRowStart: 2, gridRowEnd: 3, gridColumnStart: 2, gridColumnEnd: 3, margin: "1em", borderRadius: "1em"}} src={st3step0Gif}/>
     </div>
     <div style={{gridRowStart: 2, gridColumnStart: 1, display: "flex", flexDirection: "column"}}> {/*info*/}
       <ul style={{color: "white", fontSize: "3em", marginLeft: "0.4em"}}>

b93b2acc35f3fb6a3bbb7b3e38695658d1ea3dd7
Reuben 'Aurah' Harris <AurahTF@users.noreply.github.com>
2024-03-13 14:37:30 +1300

    all gifs

--- src/WizardSteps/st1/St1step8.tsx
+++ src/WizardSteps/st1/St1step8.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import st1step8Gif from "../../Gif/st1step8.gif"
 
 import Button from '@mui/material/Button';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
@@ -15,11 +16,15 @@ export default (props: {
   return (
     <>
     <StepHeader>specify charger connetivity method</StepHeader>
-    <h2 style={{color: "white", gridRowStart: 3, gridColumnStart: 1, gridColumnEnd: 3, textAlign: "center"}}>If Unsure Select Yes</h2>
-    <h3 style={{color: "white", gridRowStart: 4, gridColumnStart: 1, gridColumnEnd: 3, textAlign: "center"}}>Click Yes or No to Continue</h3>
+    <h2 style={{color: "white", gridRowStart: 3, gridColumnStart: 1, gridColumnEnd: 3, textAlign: "center"}}>Specify in Custom Field "Charger Connectivity"</h2>  
+    <img style={{gridRowStart: 2, gridRowEnd: 3, gridColumnStart: 2, gridColumnEnd: 3, margin: "1em", borderRadius: "1em"}} src={st1step8Gif} />
+    <h3 style={{color: "white", gridRowStart: 4, gridColumnStart: 1, gridColumnEnd: 3, textAlign: "center"}}>Select Option to Continue</h3>
     <div style={{color: "white", gridRowStart: 5, gridColumnStart: 1, gridColumnEnd: 3, display: "flex", flexDirection: "row", justifyContent: "center", textAlign: "center"}}>
       <div>
         <Button variant="contained" endIcon={<ArrowForwardIcon />} style={{width: "auto", margin: "1em", fontSize: "1.5em", fontWeight: "700"}} onClick={() => props.wizard.goToStep("sim1step1")}>SIM</Button>
+      </div>
+      <div>
+        <Button variant="contained" endIcon={<ArrowForwardIcon />} style={{width: "auto", margin: "1em", fontSize: "1.5em", fontWeight: "700"}} onClick={() => props.wizard.goToStep("sim1step1")}>Wifi Existing</Button>
       </div>
       <div>
         <Button variant="contained" endIcon={<ArrowForwardIcon />} style={{width: "auto", margin: "1em", fontSize: "1.5em", fontWeight: "700"}} onClick={() => props.wizard.goToStep("del1step1")}>Multiple</Button>
--- src/WizardSteps/st1/del1/Del1step1.tsx
+++ src/WizardSteps/st1/del1/Del1step1.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import createGif from "../../../Gif/create.gif";
+import del1step1Gif from "../../../Gif/del1step1.gif";
 
 import Button from '@mui/material/Button';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
@@ -17,7 +17,7 @@ export default (props: {
     <>
       <StepHeader>specify in description which chargers are what connectivity method</StepHeader>
       <div style={{display: "flex", marginLeft: "auto", alignContent: "center", justifyContent: "center", alignSelf: "center"}}>
-        <img style={{gridRowStart: 2, gridRowEnd: 3, gridColumnStart: 2, gridColumnEnd: 3, margin: "1em", borderRadius: "1em"}} src={createGif} />
+        <img style={{gridRowStart: 2, gridRowEnd: 3, gridColumnStart: 2, gridColumnEnd: 3, margin: "1em", borderRadius: "1em"}} src={del1step1Gif} />
       </div>
       <div style={{gridRowStart: 2, gridColumnStart: 1, display: "flex", flexDirection: "column"}}>
         <ul style={{color: "white", fontSize: "4em", marginLeft: "0.4em"}}>

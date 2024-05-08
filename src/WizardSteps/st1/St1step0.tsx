a1a201e3121800897270c0835f1bfd3ecbc5c0bc
reuben-thundergrid <129225193+reuben-thundergrid@users.noreply.github.com>
2024-05-08 19:47:50 +1200

    Update St1step0.tsx
    spelling

--- src/WizardSteps/st1/St1step0.tsx
+++ src/WizardSteps/st1/St1step0.tsx
@@ -21,7 +21,7 @@ export default (props: {
       </div>
       <div style={{gridRowStart: 2, gridColumnStart: 1, display: "flex", flexDirection: "column"}}>
         <ul style={{color: "white", fontSize: "3.5em", marginLeft: "0.4em"}}>
-          <li>Create a new task inside Network Ops ➡️ Ocean Connections ➡️ Main</li>
+          <li>Create a new task inside Ocean Plus ➡️ Ocean Connections ➡️ Main</li>
           <li>Mention this task inside the Sales task description under the "Ocean Connection(s)" text</li>
           <li style={{border: "1px solid red", borderRadius: "15px", paddingLeft: "1em", paddingBottom: "0.2em"}}>Create a separate ocean connection for each cluster</li>
           <li>The name of the task will become the name in Ocean</li>

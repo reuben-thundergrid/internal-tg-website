61610cd254d34f7a771e6d79ca4adee936066f20
Reuben <reuben.andrews@thundergrid.net>
2024-06-25 11:50:34 +1200

    added mix1 back

---
+++ src/WizardSteps/st1/mix1/Mix1step1.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import lm1step0Gif from "../../../Gif/lm1step0.gif";
+
+import StepHeader from "../../../Components/StepHeader.tsx"
+
+import { IWizard } from "use-wizard/lib/cjs/useWizard/types/IWizard";
+import { TStep } from "use-wizard/lib/cjs/useWizard/types/TStep";
+
+const Mixstep1 = (props: {
+  step: TStep;
+  wizard: IWizard;
+}) => {
+  return (
+    <>
+      <StepHeader>Mixture Local + Static</StepHeader>
+      <div style={{display: "flex", marginLeft: "auto", alignContent: "center", justifyContent: "center", alignSelf: "center"}}>
+        <img style={{gridRowStart: 2, gridRowEnd: 3, gridColumnStart: 2, gridColumnEnd: 3, margin: "1em", borderRadius: "1em"}} src={lm1step0Gif} alt="ClickUp GIF"/>
+      </div>
+      <div style={{gridRowStart: 2, gridColumnStart: 1, display: "flex", flexDirection: "column"}}>
+        <ul style={{color: "white", marginLeft: "0.4em"}}>
+          <li>Specify in the description how many chargers are to be statically configured</li>
+          <li>If a lower current than the default is be set specify this as well</li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Mixstep1;

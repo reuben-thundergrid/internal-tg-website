55b678e07380775f03821c4d2b84d8151a153105
Reuben <reuben.andrews@thundergrid.net>
2024-06-25 12:31:42 +1200

    added step69

---
+++ src/WizardSteps/st1/St1step69.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import st1step0Gif from "../../Gif/st1step0.gif";
+
+import StepHeader from "../../Components/StepHeader.tsx"
+
+import { IWizard } from "use-wizard/lib/cjs/useWizard/types/IWizard";
+import { TStep } from "use-wizard/lib/cjs/useWizard/types/TStep";
+
+const St1step69 = (props: {
+  step: TStep;
+  wizard: IWizard;
+}) => {
+  return (
+    <>
+      <StepHeader>specify ocean instance</StepHeader>
+      <div style={{display: "flex", marginLeft: "auto", alignContent: "center", justifyContent: "center", alignSelf: "center"}}>
+        <img style={{gridRowStart: 2, gridRowEnd: 3, gridColumnStart: 2, gridColumnEnd: 3, margin: "1em", borderRadius: "1em"}} src={st1step0Gif} alt="ClickUp GIF" />
+      </div>
+      <div style={{gridRowStart: 2, gridColumnStart: 1, display: "flex", flexDirection: "column"}}>
+        <ul style={{color: "white", marginLeft: "0.4em", display: "flex", flexDirection: "column"}}>
+          <li>Specify which country the chargers are going into in the custom field "Instance"</li>
+          <li>This determines which Ocean instance the chargers will get added too</li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default St1step69;

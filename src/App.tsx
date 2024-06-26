edb7f16dc52210cc3a0c92425e0de54cc6664ea6
Reuben 'Aurah' Harris <AurahTF@users.noreply.github.com>
2024-06-26 17:08:18 +1200

    added meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,16 @@ const App = () => {
           <Route path="*" element={<h1 style={{color: "white"}}>Page Doesn't Exist</h1>} />
         </Routes>
       </BrowserRouter>
+      {/* <!-- Primary Meta Tags --> */}
+      <title>Thundergrid Click</title>
+      <meta name="title" content="Thundergrid Click" />
+      <meta name="description" content="Company hub for internal processes" />
+
+      {/* <!-- Open Graph / Facebook --> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="http://thundergrid.click" />
+      <meta property="og:title" content="Thundergrid Click" />
+      <meta property="og:description" content="Company hub for internal processes" />
     </>
   );
 };

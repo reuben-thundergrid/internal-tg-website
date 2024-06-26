2814ee790a7832ce39a64547252612a2ee6f1443
Reuben 'Aurah' Harris <AurahTF@users.noreply.github.com>
2024-06-26 15:55:33 +1200

    hidden home button on home page

--- src/Components/StepHeader.tsx
+++ src/Components/StepHeader.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import HouseIcon from '@mui/icons-material/House';
 import { Button } from "@mui/material";
@@ -9,19 +9,26 @@ const StepHeader = ({children}) => {
     const navigate = useNavigate();
     function handleHome() { navigate("/home"); }
 
+    const location = useLocation();
+    console.log(location.pathname);
+
     return (
-        <div style={{gridRowStart: 1, gridColumnStart: 1, gridColumnEnd: 3, display: "flex", alignItems: "center", justifyContent: "space-between"}}>
-                {/* Hidden button to keep the h1 centered */}
-                <Button variant="contained" endIcon={<HouseIcon />}style={{fontWeight:"600", marginLeft: "1em", visibility: "hidden"}}>Home</Button>
-                <h1 className="StepHeader" style={{color: "white", textAlign: "center"}} >{children}</h1>
-                <Button 
-                    variant="contained" 
-                    endIcon={<HouseIcon />} 
-                    onClick={() => handleHome()}
-                    style={{fontWeight:"600", height: "50%", marginRight: "1em"}}
-                    >Home
-                </Button>
-        </div>
+        <>
+            {/* Duplicate element otherwise h1 will go to the left with the stlying it has if buttons are turned off */}
+            { location.pathname !== "/wizard" ? <h1 className="StepHeader" style={{color: "white", textAlign: "center"}} >{children}</h1> : <></> }
+            <div style={{gridRowStart: 1, gridColumnStart: 1, gridColumnEnd: 3, display: "flex", alignItems: "center", justifyContent: "space-between"}}>
+                    {/* Hidden button to keep the h1 centered */}
+                    { location.pathname === "/wizard" ? <Button variant="contained" endIcon={<HouseIcon />}style={{fontWeight:"600", marginLeft: "1em", visibility: "hidden"}}>Home</Button> : <></> }
+                    { location.pathname === "/wizard" ? <h1 className="StepHeader" style={{color: "white", textAlign: "center"}} >{children}</h1> : <></> }
+                    { location.pathname === "/wizard" ? <Button 
+                        variant="contained" 
+                        endIcon={<HouseIcon />} 
+                        onClick={() => handleHome()}
+                        style={{fontWeight:"600", height: "50%", marginRight: "1em"}}
+                        >Home
+                    </Button> : <></> }
+            </div>
+        </>
     );
 }
 
